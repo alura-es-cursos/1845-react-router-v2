1be49f64aeae411ca14e7f423f4771f79d6a33b1
Harland Lohora <harland@lohora.com>
2023-03-20 05:55:19 -0600

    clase01 completada

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
+import Home from "./pages/Home"
+import Sobre from "./pages/Sobre"
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <>
 
-    </>
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sobre' element={<Sobre />} />
+      </Routes>
+    </Router>
   );
 }
 
