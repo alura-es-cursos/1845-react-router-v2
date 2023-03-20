ba13a3c2c323ffdc972a015fabc5dff2e704e785
Harland Lohora <harland@lohora.com>
2023-03-19 23:03:14 -0600

    Codigo inicial

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+const Home = () => {
+  return (
+    <main>
+      <div className="container">
+        <h2 className="title-page">Pet noticias</h2>
+      </div>
+      <section className="container flex flex--center">
+        <article className="card post">
+          <h2 className="post-card__title">Baño en perros</h2>
+          <p className="post-card__text">La mayoría de los perros prefieren saltarse la hora del baño, pero el baño juega un papel importante en la salud del pelaje y la piel de su perro, ya que ayuda a mantenerla limpia y libre de suciedad y parásitos. Tener los productos adecuados puede ayudarlo a sentirse más cómodo.</p>
+        </article>
+      </section>
+    </main>
+  )
+}
+
+export default Home
---
+++ src/pages/Sobre.jsx
@@ -0,0 +1,16 @@
+const Sobre = () => {
+  return (
+    <main>
+      <div className="container">
+        <h2 className="title-page">Sobre Doguito</h2>
+      </div>
+      <section className="container flex flex--center">
+        <article className="card">
+          El blog de PetShop fue creado para ayudarte con las preguntas más comunes sobre tu mascota.
+        </article>
+      </section>
+    </main>
+  )
+}
+
+export default Sobre
